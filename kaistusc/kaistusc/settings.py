1c07be3af57c6517786f907916945a7852cb453c
Hang Park <hangpark@kaist.ac.kr>
2016-12-28 05:46:07 +0900

    Refs #2 -- Initialize settings to use i18n
    settings.py에 locale middleware와 locale path를 추가하였다.

--- kaistusc/kaistusc/settings.py
+++ kaistusc/kaistusc/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'kaistusc.urls'
@@ -113,6 +114,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, "locale"),
+]
 
 
 # Static files (CSS, JavaScript, Images)
