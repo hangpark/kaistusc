10ebed87ec6bfa018aedf9718750011c095f0625
Hang Park <hangpark@kaist.ac.kr>
2016-12-28 06:49:11 +0900

    Fixed #3 -- Change ugettext to ugettext_lazy
    manager.models에서 `ugettext` 대신 `ugettext_lazy`를 사용하도록 함.
    
    참고 :
    https://docs.djangoproject.com/en/1.10/topics/i18n/translation/#lazy-translation

--- kaistusc/manager/models.py
+++ kaistusc/manager/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class Category(models.Model):
