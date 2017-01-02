6196abead1cae9d6324bb75fe761f8563bfd975a
Hang Park <hangpark@kaist.ac.kr>
2017-01-03 03:18:06 +0900

    Resolved #5 -- Bower & Gulp initial settings
    frontend 디렉토리 안에 package.json, bower.json, gulpfile.js의 기본 틀을
    설정하였다.

---
+++ kaistusc/frontend/gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var src = './src';
+var dist = './dist';
+var bower = './bower_components';
+
+var jquery = bower + '/jquery/dist';
+var bootstrap = bower + '/bootstrap-sass/assets';
+
+var js = {
+	'in': [
+		jquery + '/jquery.js',
+		bootstrap + '/javascripts/bootstrap.js'
+	],
+	'out': dist + '/js'
+};
+
+var fonts = {
+	'in': bootstrap + '/fonts/**/*',
+	'out': dist + '/fonts'
+};
+
+var scss = {
+	'in': src + '/main.scss',
+	'out': dist + '/css',
+	'opts': {
+		errLogToConsole: true,
+		includePaths: [bootstrap + '/stylesheets']
+	}
+};
+
+gulp.task('js', function() {
+	return gulp.src(js.in)
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(js.out));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src(fonts.in)
+		.pipe(gulp.dest(fonts.out));
+});
+
+gulp.task('scss', ['fonts'], function() {
+	return gulp.src(scss.in)
+		.pipe(sass(scss.opts))
+		.pipe(postcss([ autoprefixer() ]))
+		.pipe(minifycss())
+		.pipe(gulp.dest(scss.out));
+});
+
+gulp.task('default', ['js', 'fonts', 'scss']);
--- kaistusc/kaistusc/settings.py
+++ kaistusc/kaistusc/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "frontend/dist"),
 )
 
 STATIC_ROOT = private_settings.STATIC_ROOT
