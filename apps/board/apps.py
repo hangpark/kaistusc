bf8942b86842b09d33b51e763c9b88a74d33085d
Hang Park <hangpark@kaist.ac.kr>
2017-01-27 12:27:14 +0900

    Refs #40 -- Add meta infos of board app in apps.py

--- apps/board/apps.py
+++ apps/board/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class BoardConfig(AppConfig):
-    name = 'board'
+    name = 'apps.board'
+    verbose_name = _("게시판")
