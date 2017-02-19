27e9289df366b8d72f9b22e2be04575bd4ed11bd
Hang Park <hangpark@kaist.ac.kr>
2017-02-19 21:40:55 +0900

    Fixed #78 -- Return empty string when parsing fails

--- apps/ksso/classes.py
+++ apps/ksso/classes.py
@@ -1,3 +1,9 @@
+from xml.etree import ElementTree
+
+import requests
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
 from . import settings
 from .models import PortalInfo
 
@@ -25,7 +31,7 @@ class PortalController():
             # KAIST 단일서비스 인증정보가 저장되어 있는 경우
             # XML 정보를 바탕으로 database를 갱신합니다.
             try:
-                self.portal_info = PortalInfo.all_objects.get(
+                self.portal_info = PortalInfo.objects.get(
                     kaist_uid=self.kaist_uid)
                 self.update_portal_info()
 
@@ -37,7 +43,6 @@ class PortalController():
 
         # PortalInfo instance를 생성합니다.
         def insert_portal_info(self):
-            from django.contrib.auth.models import User
             user = User.objects.create_user(username=self.kaist_uid,
                                             password=self.kaist_uid)
             user.save()
@@ -47,23 +52,18 @@ class PortalController():
         # PortalInfo를 갱신합니다.
         def update_portal_info(self):
             self.portal_info.kaist_uid = self.parser.attr('kaist_uid')
-            self.portal_info.ku_kname = self.parser.attr('ku_kname')
-            self.portal_info.ku_acad_prog = self.parser.attr('ku_acad_prog')
-            self.portal_info.ku_std_no = self.parser.attr('ku_std_no')
-            self.portal_info.ku_psft_user_status_kor = self.parser.attr(
-                'ku_psft_user_status_kor')
-            self.portal_info.ku_born_date = self.parser.attr('ku_born_date')
-            self.portal_info.ku_sex = self.parser.attr('ku_sex')
-            self.portal_info.ou = self.parser.attr('ou')
-            self.portal_info.mail = self.parser.attr('mail')
-            self.portal_info.mobile = self.parser.attr('mobile')
+
+            fields = [
+                'ku_kname', 'ku_acad_prog', 'ku_std_no', 'ku_born_date'
+                'ku_psft_user_status_kor', 'ku_sex', 'ou', 'mail', 'mobile']
+            for field in fields:
+                setattr(self.portal_info, field, self.parser.attr(field))
 
             self.portal_info.save()
 
         def session(self):
             if self.portal_info is None:
                 return None
-            from django.contrib.auth import authenticate
             user = authenticate(username=self.portal_info.user.username,
                                 password=self.portal_info.user.username)
             return user
@@ -71,7 +71,6 @@ class PortalController():
         class PortalParser():
 
             def __init__(self, user_data):
-                from xml.etree import ElementTree
                 self.data = ElementTree.fromstring(user_data.text)
                 self.data = self.data.getchildren()[0]
                 self.data = self.data.getchildren()[0]
@@ -79,7 +78,7 @@ class PortalController():
 
             # XML 정보를 파싱하여서 리턴합니다.
             def attr(self, item):
-                return self.data.findtext(item)
+                return self.data.findtext(item) or ""
 
     class AuthController():
 
@@ -90,25 +89,28 @@ class PortalController():
 
         # request string 을 만듭니다.
         def build_request_string(self):
-            request_string = '<soapenv:Envelope xmlns:soapenv='
-            request_string += '"http://schemas.xmlsoap.org/soap/envelope/"'
-            request_string += ' xmlns:ser="http://server.com">'
-            request_string += '<soapenv:Header/>'
-            request_string += '<soapenv:Body>'
-            request_string += '<ser:verification>'
-            request_string += '<cookieValue>' + self.token
-            request_string += '</cookieValue>'
-            request_string += '<publicKeyStr>' + settings.PORTAL_PUBLIC_KEY
-            request_string += '</publicKeyStr>'
-            request_string += '<adminVO>'
-            request_string += '<adminId>' + settings.PORTAL_ADMIN_ID
-            request_string += '</adminId>'
-            request_string += '<password>' + settings.PORTAL_ADMIN_PW
-            request_string += '</password>'
-            request_string += '</adminVO>'
-            request_string += '</ser:verification>'
-            request_string += '</soapenv:Body>'
-            request_string += '</soapenv:Envelope>'
+            request_string = (
+                '<soapenv:Envelope xmlns:soapenv='
+                '"http://schemas.xmlsoap.org/soap/envelope/"'
+                ' xmlns:ser="http://server.com">'
+                '<soapenv:Header/>'
+                '<soapenv:Body>'
+                '<ser:verification>'
+                '<cookieValue>{token}</cookieValue>'
+                '<publicKeyStr>{public_key}</publicKeyStr>'
+                '<adminVO>'
+                '<adminId>{admin_id}</adminId>'
+                '<password>{admin_pw}</password>'
+                '</adminVO>'
+                '</ser:verification>'
+                '</soapenv:Body>'
+                '</soapenv:Envelope>'
+            ).format(
+                token=self.token,
+                public_key=settings.PORTAL_PUBLIC_KEY,
+                admin_id=settings.PORTAL_ADMIN_ID,
+                admin_pw=settings.PORTAL_ADMIN_PW
+            )
             return request_string
 
         # request header 를 만듭니다.
@@ -124,7 +126,6 @@ class PortalController():
 
         # requests를 이용하여 연결합니다.
         def connect(self):
-            import requests
             response = requests.post(
                 settings.PORTAL_TARGET_URL,
                 data=self.request_string,
