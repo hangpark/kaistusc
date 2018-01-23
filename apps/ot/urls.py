20b0f65842f8f53eb73861807779c744c95cc076
namsan <namsan@kaist.ac.kr>
2018-01-23 17:35:00 +0900

    투표 결과 페이지 추가

--- apps/ot/urls.py
+++ apps/ot/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 
 from .views.main import MainView
 from .views.club import ClubDetailView, ClubListView
-from .views.user import TSizeView
+from .views.user import TSizeView, ResultView
 
 urlpatterns = [
     url(r'^$', MainView.as_view(), name='main'),
+    url(r'^result/$', ResultView.as_view()),
     url(r'^tshirt/$', TSizeView.as_view(), name='t_size'),
     url(r'^club/$', ClubListView.as_view(), name='club_list'),
     url(r'^club/(?P<pk>\d+)/$', ClubDetailView.as_view(), name='club_detail'),
