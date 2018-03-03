635aa9cdf3536e3f3723daaee344cd3eb4b3de61
Stein Kim <wotjd1102@gmail.com>
2018-03-04 00:09:31 +0900

    Code review applied
    다음의 코드리뷰 사항 적용되었습니다:
    1. 인라인 스타일 제거
    2. 국제화 적용
    3. 리로드 대신 dom 조작으로 삭제 적용.

--- static/src/javascripts/board_banner.js
+++ static/src/javascripts/board_banner.js
@@ -1,14 +1,34 @@
 $().ready(function() {
   $("#delete-board-banner").click(function() {
     var boardBannerContext = document.getElementById('delete-board-banner-context')
-    if (confirm("게시판 배너를 삭제하시겠습니까?")) {
+    if (confirm(boardBannerContext.getAttribute('data-confirm-msg'))) {
       $.ajax({
           url: boardBannerContext.getAttribute('data-url'),
           type: 'POST',
           data: { csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val() },
       })
       .done(function(data) {
-        location.reload();
+        var boardBanner = document.getElementById('board-banner');
+        boardBanner.removeChild(boardBanner.firstChild);
+        var pullRight = boardBanner.firstChild;
+        while (pullRight.firstChild) {
+          pullRight.removeChild(pullRight.firstChild);
+        }
+        boardBanner.insertBefore(
+          $.parseHTML(
+            '<b>' + 
+            boardBannerContext.getAttribute('data-no-banner-msg') + 
+            '</b>'
+          ), 
+          pullRight
+        )
+        pullRight.appendChild($.parseHTML(
+          '<a href="' + 
+          boardBannerContext.getAttribute('current-url') + 
+          '/banner/new/">' + 
+          boardBannerContext.getAttribute('data-register-text') + 
+          '</a>')
+        )
       })
       .fail(function(data) {
         alert(data.message);
